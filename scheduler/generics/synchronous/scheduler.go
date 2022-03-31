bfa5190832a21540ad9b439aa63ad883e521f7f2
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-03-31 21:33:49 +0100

    Add lazy func type

--- scheduler/generics/synchronous/scheduler.go
+++ scheduler/generics/synchronous/scheduler.go
@@ -1,19 +1,15 @@
 package synchronous
 
-type permittedValues interface {
-	~int | ~float64 | ~string
-}
-
-//type Scheduler[T permittedValues] interface {
-//	Size() int
-//	Add(fn LazyFunc, args ...T)
-//	Run() []Result[T]
-//}
+type LazyFunc[T permittedValues] func(n ...T) T
 
 type Scheduler[T permittedValues] interface {
 	Size() int
-	Add(fn func(n ...T) T, args ...T)
+	Add(fn LazyFunc[T], args ...T)
 	Run() []Result[T]
+}
+
+type permittedValues interface {
+	~int | ~float64 | ~string
 }
 
 type Result[T permittedValues] struct {
@@ -21,16 +17,9 @@ type Result[T permittedValues] struct {
 	Err   error
 }
 
-type LazyScheduler[T permittedValues] struct {
+type lazyScheduler[T permittedValues] struct {
 	jobs []job[T]
 }
-
-type LazyFunc[T permittedValues] func(n ...T) T
-
-//type job[T permittedValues] struct {
-//	lazyFn LazyFunc[T]
-//	args   []T
-//}
 
 type job[T permittedValues] struct {
 	lazyFn func(n ...T) T
@@ -38,30 +27,20 @@ type job[T permittedValues] struct {
 }
 
 func New[T permittedValues]() Scheduler[T] {
-	return &LazyScheduler[T]{
+	return &lazyScheduler[T]{
 		jobs: []job[T]{},
 	}
 }
 
-//func New[T permittedValues]() *LazyScheduler[T] {
-//	return &LazyScheduler[T]{
-//		jobs: []job[T]{},
-//	}
-//}
-
-func (s *LazyScheduler[T]) Size() int {
+func (s *lazyScheduler[T]) Size() int {
 	return len(s.jobs)
 }
 
-//func (s *LazyScheduler[T]) Add(fn LazyFunc, args ...T) {
-//	s.jobs = append(s.jobs, job[T]{lazyFn: fn, args: args})
-//}
-
-func (s *LazyScheduler[T]) Add(fn func(n ...T) T, args ...T) {
+func (s *lazyScheduler[T]) Add(fn LazyFunc[T], args ...T) {
 	s.jobs = append(s.jobs, job[T]{lazyFn: fn, args: args})
 }
 
-func (s *LazyScheduler[T]) Run() []Result[T] {
+func (s *lazyScheduler[T]) Run() []Result[T] {
 	res := make([]Result[T], s.Size())
 
 	for i, job := range s.jobs {
